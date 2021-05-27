684c33453526f94de106efd74e745b3b9fe987ad
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-05-27 13:08:22 -0500

    project pablo finish

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 const menuIcon = document.querySelector(".menu-icon");
 const menu = document.querySelector(".menu");
+const links = document.querySelectorAll(".menu-link");
 
-function showMenu(e) {
+function showMenu() {
   menuIcon.classList.toggle("animate");
   menu.classList.toggle("show");
 }
 
+function addFocus() {
+  links.forEach((link) => link.classList.remove("active"));
+  this.classList.add("active");
+}
+
 menuIcon.addEventListener("click", showMenu);
+links.forEach((link) => link.addEventListener("click", addFocus));
